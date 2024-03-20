90f1a2d8e104ba257bf6d9db35589e000f2f39d2
Oh...Yeah!!! <614988210@qq.com>
2024-03-20 19:05:27 +0800

    props传递多类型的数据

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,36 @@
     我是APP组件 {{ nowTitle }}
     <!-- 1.给组件标签，添加属性方式 赋值 -->
     <Son :title="myTitle" @changeTitle="handleChange"></Son>
+    <UserInfo
+      :username="username"
+      :age="age"
+      :isSingle="isSingle"
+      :car="car.brand"
+      :hobby="hobby"
+    
+    ></UserInfo>
+
   </div>
 </template>
 
 <script>
 import Son from './components/Son.vue'
+import UserInfo from './components/UserInfo.vue';
 export default {
   name: 'App',
   data() {
     return {
       myTitle: '学前端，就多练习',
-      nowTitle: ''
+      nowTitle: '',
+
+      username: '小帅',
+      age: 28,
+      isSingle: true,
+      car: {
+        brand: '宝马'
+      },
+      hobby: ['篮球','足球','羽毛球']
+
     }
   },
   methods: {
@@ -23,7 +42,7 @@ export default {
     }
   },
   components: {
-    Son
+    Son,UserInfo
   },
 }
 </script>
