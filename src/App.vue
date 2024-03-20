1ab3eaf2312a254c0a2436d2e83132071d7498da
Oh...Yeah!!! <614988210@qq.com>
2024-03-20 16:53:00 +0800

    实现父传子，子传父的练习

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <div id="app">
-    <BaseOne></BaseOne>
-    <BaseTwo></BaseTwo>
-    
-    <BaseTwo></BaseTwo>
-    <BaseTwo></BaseTwo>
+  <div class="app" style="border: 3px solid #000; margin: 10px">
+    我是APP组件 {{ nowTitle }}
+    <!-- 1.给组件标签，添加属性方式 赋值 -->
+    <Son :title="myTitle" @changeTitle="handleChange"></Son>
   </div>
 </template>
 
 <script>
-import BaseOne from './components/BaseOne.vue'
-import BaseTwo from './components/BaseTwo.vue'
+import Son from './components/Son.vue'
 export default {
   name: 'App',
+  data() {
+    return {
+      myTitle: '学前端，就多练习',
+      nowTitle: ''
+    }
+  },
+  methods: {
+    //3.提供处理函数，提供逻辑
+    handleChange(newTitle){
+      this.nowTitle = newTitle
+    }
+  },
   components: {
-    BaseOne,
-    BaseTwo
-  }
+    Son
+  },
 }
 </script>
 
-
+<style></style>
