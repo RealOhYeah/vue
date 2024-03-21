0804f2fe19794ba469824d00497bb20e8826bb5a
Oh...Yeah!!! <614988210@qq.com>
2024-03-21 23:21:30 +0800

    非父子通信-事件总线测试

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,22 @@
 <template>
-  <!-- 主体区域 -->
-  <section id="app">
-
-    <TodoHeader @add="handleAdd"></TodoHeader>
-    <TodoMain :list="list" @del="handleDel"></TodoMain>
-    <TodoFooter :total=list.length @clearAll="handleClearAll" ></TodoFooter>
-
-  </section>
+  <div class="app">
+    <BaseA></BaseA>
+    <BaseB></BaseB> 
+    <BaseC></BaseC>
+  </div>
 </template>
 
 <script>
-import TodoFooter  from './components/TodoFooter.vue'
-import TodoHeader  from './components/TodoHeader.vue'
-import TodoMain  from './components/TodoMain.vue'
- 
-// 渲染功能：
-// 1.提供数据： 提供在公共的父组件 App.vue
-// 2.通过父传子，将数据传递给TodoMain
-// 3.利用 v-for渲染
-
-const fruitLocal = JSON.parse(localStorage.getItem('list')) 
-const defaultArr =  [
-        { id: 1 , name: '吃饭'},
-        { id: 2 , name: '逛街'},
-        { id: 3 , name: '看电影'}  
-      ]
+import BaseA from './components/BaseA.vue'
+import BaseB from './components/BaseB.vue' 
+import BaseC from './components/BaseC.vue' 
 
 export default {
-
-  data () {
-    return {
-      
-      list:   ( !fruitLocal || fruitLocal.length === 0) ? defaultArr : fruitLocal,
-    
-    }
-  },
-  components: {
-    TodoFooter ,TodoMain ,TodoHeader 
-  },
-
-  watch: {
-     list: {
-      deep: true,
-      handler (newValue){
-        localStorage.setItem('list',JSON.stringify(newValue))
-      }
-     }
-  },
-  methods: {
-    handleAdd(todoName){
-
-
-      this.list.push(
-        {
-          id: +new Date(),
-          name: todoName
-      }
-      )
-    },
-    handleDel(index){
-      this.list.splice(index,1)
-    },
-    handleClearAll(){
-        this.list = [];
-    }
-
-
+  components:{
+    BaseA,
+    BaseB,
+    BaseC
+     
   }
 }
 </script>
