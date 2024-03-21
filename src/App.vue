a98917a6ed86d5a089bfaa3fbf297babab6eab14
Oh...Yeah!!! <614988210@qq.com>
2024-03-21 19:03:09 +0800

    小黑记事本，实现数据的增删、清空所有、统计总数的功能

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,61 @@
 <template>
-  <div class="app" style="border: 3px solid #000; margin: 10px">
-    我是APP组件 {{ nowTitle }}
-    <!-- 1.给组件标签，添加属性方式 赋值 -->
-    <Son :title="myTitle" @changeTitle="handChange"></Son>
-    <UserInfo
-      :username="username"
-      :age="age"
-      :isSingle="isSingle" 
-      :car="car.brand"
-      :hobby="hobby"
-    
-    ></UserInfo>
+  <!-- 主体区域 -->
+  <section id="app">
 
-    <br><br><br>
+    <TodoHeader @add="handleAdd"></TodoHeader>
+    <TodoMain :list="list" @del="handleDel"></TodoMain>
+    <TodoFooter :total=list.length @clearAll="handleClearAll" ></TodoFooter>
 
-    <BaseProgress :w="count"></BaseProgress>
-
-    <br><br><br>
-
-    <BaseCount :count="count" @changeCount="handleChange"></BaseCount>
-
-
-  </div>
+  </section>
 </template>
 
 <script>
-import Son from './components/Son.vue'
-import UserInfo from './components/UserInfo.vue';
-import BaseProgress from './components/BaseProgress.vue'
-import BaseCount from './components/BaseCount.vue'
+import TodoFooter  from './components/TodoFooter.vue'
+import TodoHeader  from './components/TodoHeader.vue'
+import TodoMain  from './components/TodoMain.vue'
+ 
+// 渲染功能：
+// 1.提供数据： 提供在公共的父组件 App.vue
+// 2.通过父传子，将数据传递给TodoMain
+// 3.利用 v-for渲染
+
 export default {
-  name: 'App',
-  data() {
+  data () {
     return {
-      myTitle: '学前端，就多练习',
-      nowTitle: '',
-
-      username: '小帅',
-      age: 28,
-      isSingle: true,
-      car: {
-        brand: '宝马'
-      },
-      hobby: ['篮球','足球','羽毛球'],
-
-      // width: 30,
-      count: 100
+      list:[
+        { id: 1 , name: '吃饭'},
+        { id: 2 , name: '逛街'},
+        { id: 3 , name: '看电影'}  
+      ],
+    
     }
-  },
-  methods: {
-    //3.提供处理函数，提供逻辑
-    handChange(newTitle){
-      this.nowTitle = newTitle
-    },
-    handleChange(newCount){
-      console.log(newCount);
-      this.count = newCount
-      this.width = this.count
-    }
-
   },
   components: {
-    Son,UserInfo,BaseProgress,BaseCount
+    TodoFooter ,TodoMain ,TodoHeader 
   },
+  methods: {
+    handleAdd(todoName){
+
+
+      this.list.push(
+        {
+          id: +new Date(),
+          name: todoName
+      }
+      )
+    },
+    handleDel(index){
+      this.list.splice(index,1)
+    },
+    handleClearAll(){
+        this.list = [];
+    }
+
+
+  }
 }
 </script>
 
-<style></style>
+<style>
+
+</style>
---
+++ src/components/TodoMain.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- 列表区域 -->
+  <section class="main">
+    <ul class="todo-list">
+      <li class="todo" v-for="(item, index) in list" :key="item.id" >
+        <div class="view">
+          <span class="index">{{ index +1 }}.</span> <label>{{ item.name  }}</label>
+          <button @click="handleDel(index)" class="destroy"  ></button>
+        </div>
+      </li>
+    </ul>
+  </section>
+
+</template>
+
+<script>
+export default {
+  props: {
+    list:Array
+  },
+  methods: {
+    handleDel(index){
+      this.$emit('del',index)
+    }
+  }
+}
+</script>
+
+<style></style>
