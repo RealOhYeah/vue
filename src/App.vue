992ee3ae963af1b71994fd9e9b995d2b038e9f7b
Oh...Yeah!!! <614988210@qq.com>
2024-03-22 22:54:17 +0800

    ref和$refs实现组件实例

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="app">
-    <div class="base-chart-box">
-      这是一个捣乱的盒子
-    </div>
-    <BaseChart></BaseChart>
+    <h4>父组件 -- <button>获取组件实例</button></h4>
+    <BaseForm ref="BaseForm"></BaseForm>
+    
+    <button @click="handleGet">获取数据</button>
+    <button @click="handleReset">重置数据</button>
   </div>
 </template>
 
-<script>
-import BaseChart from './components/BaseChart.vue'
+<script> 
+import BaseForm from './components/BaseForm.vue'
 export default {
-  components:{
-    BaseChart
+  components: {
+    BaseForm,
+  },
+  methods: {
+   handleGet(){
+    
+    // console.log(this.$refs.BaseForm.getFormData);
+    console.log(this.$refs.BaseForm.getFormData());
+
+
+   },
+   handleReset(){
+    this.$refs.BaseForm.resetFormData();
+   }
   }
 }
 </script>
 
 <style>
-.base-chart-box {
-  width: 300px;
-  height: 200px;
-}
 </style>
