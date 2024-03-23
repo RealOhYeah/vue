bd0bb06748759a71c999bd04dccaf90d04888bee
Oh...Yeah!!! <614988210@qq.com>
2024-03-23 15:45:25 +0800

    vue的插槽使用

--- src/App.vue
+++ src/App.vue
@@ -1,127 +1,50 @@
 <template>
-  <div class="main">
-    <div class="box" v-loading="isLoading">
-      <ul>
-        <li v-for="item in list" :key="item.id" class="news">
-          <div class="left">
-            <div class="title">{{ item.title }}</div>
-            <div class="info">
-              <span>{{ item.source }}</span>
-              <span>{{ item.time }}</span>
-            </div>
-          </div>
-          <div class="right">
-            <img :src="item.img" alt="">
-          </div>
-        </li>
-      </ul>
-    </div> 
-    <div class="box2" v-loading="isLoading2"></div>
+  <div>
+    <MyTable :data="list">
+      <!-- 3. 通过template #插槽名="变量名" 接收 -->
+      <template #default="obj">
+        <button @click="del(obj.row.id)">
+          删除
+        </button>
+      </template>
+    </MyTable>
+    
+    <MyTable :data="list2">
+      <template #default="{ row }">
+        <button @click="show(row)">查看</button>
+      </template>
+    </MyTable>
   </div>
 </template>
 
 <script>
-// 安装axios =>  yarn add axios || npm i axios
-import axios from 'axios'
-
-// 接口地址：http://hmajax.itheima.net/api/news
-// 请求方式：get
+import MyTable from './components/MyTable.vue'
 export default {
   data () {
     return {
-      list: [],
-      isLoading: true,
-      isLoading2: true
+      list: [
+        { id: 1, name: '张小花', age: 18 },
+        { id: 2, name: '孙大明', age: 19 },
+        { id: 3, name: '刘德忠', age: 17 },
+      ],
+      list2: [
+        { id: 1, name: '赵小云', age: 18 },
+        { id: 2, name: '刘蓓蓓', age: 19 },
+        { id: 3, name: '姜肖泰', age: 17 },
+      ]
     }
   },
-  async created () {
-    // 1. 发送请求获取数据
-    const res = await axios.get('http://hmajax.itheima.net/api/news')
-    
-    setTimeout(() => {
-      // 2. 更新到 list 中，用于页面渲染 v-for
-      this.list = res.data.data
-      this.isLoading = false
-      this.isLoading2 = false
-      
-    }, 1000)
-  },
-  directives: {
-    loading: {
-      inserted(el,binding){
-        console.log(binding.value);
-        binding.value ? el.classList.add('loading') : el.classList.remove('loading')
-      },
-      update(el,binding) {
-        console.log(binding.value);
-        binding.value ? el.classList.add('loading') : el.classList.remove('loading')
-        
-      },
+  methods: {
+    del (id) {
+      this.list = this.list.filter(item => item.id !== id)
+    },
+    show (row) {
+      // console.log(row);
+      alert(`姓名：${row.name}; 年纪：${row.age}`)
     }
+  },
+  components: {
+    MyTable
   }
-
-
 }
 </script>
-
-<style>
-.loading:before {
-  content: '';
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background: #fff url('./loading.gif') no-repeat center;
-}
-
-.box2 {
-  width: 400px;
-  height: 400px;
-  border: 2px solid #000;
-  position: relative;
-}
-
-
-
-.box {
-  width: 800px;
-  min-height: 500px;
-  border: 3px solid orange;
-  border-radius: 5px;
-  position: relative;
-}
-.news {
-  display: flex;
-  height: 120px;
-  width: 600px;
-  margin: 0 auto;
-  padding: 20px 0;
-  cursor: pointer;
-}
-.news .left {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding-right: 10px;
-}
-.news .left .title {
-  font-size: 20px;
-}
-.news .left .info {
-  color: #999999;
-}
-.news .left .info span {
-  margin-right: 20px;
-}
-.news .right {
-  width: 160px;
-  height: 120px;
-}
-.news .right img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-</style>
