364127768d9b51add0eb2e2212eb575e6eb905ff
Oh...Yeah!!! <614988210@qq.com>
2024-03-23 11:46:31 +0800

    v-loading指令的封装

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
- 
-  // Vue.directive('focus',  {
-  //   // `bind`, `update`
-  //   inserted(el){
-  //     console.log(el);
-  //     el.focus()
-  //   }
-  // });
 
+// // 1. 全局注册指令
+// Vue.directive('focus', {
+//   // inserted 会在 指令所在的元素，被插入到页面中时触发
+//   inserted (el) {
+//     // el 就是指令所绑定的元素
+//     // console.log(el);
+//     el.focus()
+//   }
+// })
 
 
 new Vue({
